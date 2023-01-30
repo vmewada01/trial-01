34591aec64a1b810f9fb95adc6442f74137a77c5
Vishal Mewada <122343841+vmewada01@users.noreply.github.com>
2023-01-30 20:12:43 +0530

    add a comment
    adding a commment to the result part to get easy everything.

--- evenodd.js
+++ evenodd.js
@@ -1,8 +1,8 @@
 let a =7;
 
 if(a%2 ==0){
-console.log("a is even")
+console.log("a is even") // this is a commment.
 }
 else {
-console.log("a is odd")
+console.log("a is odd") // this is a comment.
 }
